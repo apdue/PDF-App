d60cadd9ac7699ca855a6fb339a30636603d4a5d
Abdul Basit Bhatti <78906762+basitbhatti@users.noreply.github.com>
2025-04-12 09:57:03 +0500

    Commit

--- app/src/main/java/com/mypdf/ocrpdfapp/PdfApplication.kt
+++ app/src/main/java/com/mypdf/ocrpdfapp/PdfApplication.kt
@@ -7,7 +7,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.util.Log
-import com.mypdf.ocrpdfapp.service.PdfForegroundService
 import com.tom_roush.pdfbox.android.PDFBoxResourceLoader
 
 class PdfApplication : Application() {
@@ -24,41 +23,13 @@ class PdfApplication : Application() {
             return applicationContext
         }
     }
-    private val CHANNEL_ID = "pdf_editor_service"
 
     override fun onCreate() {
         super.onCreate()
         // Initialize PDFBox
         Companion.applicationContext = this
         PDFBoxResourceLoader.init(applicationContext)
-        createNotificationChannel(this)
-
-        // Start the foreground service
-        startForegroundService()
     }
 
-    fun createNotificationChannel(context: Context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                CHANNEL_ID,
-                "PDF Viewer Service",
-                NotificationManager.IMPORTANCE_LOW
-            ).apply {
-                description = "This notification keeps the PDF Viewer service active."
-            }
-            val manager = context.getSystemService(NotificationManager::class.java)
-            manager.createNotificationChannel(channel)
-        }
-    }
 
-    private fun startForegroundService() {
-        Log.d("TAGNOTI", "startForegroundService: ")
-
-        val serviceIntent = Intent(this, PdfForegroundService::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(serviceIntent)
-        } else {
-            startService(serviceIntent)
-        }
-    }
 } 
